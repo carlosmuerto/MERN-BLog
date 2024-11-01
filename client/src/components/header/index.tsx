57a4f36b26053efe4c90b9d3206177a427fa1e16
Carlos A. Vivas N <cavivasnieto@gmail.com>
2024-10-31 23:36:01 -0400

    add logo searchbar/button

--- client/src/components/header/index.tsx
+++ client/src/components/header/index.tsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import { Link } from "@tanstack/react-router";
+import { Button, Navbar, TextInput } from "flowbite-react";
+import React from "react";
+import { AiOutlineSearch } from "react-icons/ai";
+import { FaMoon } from "react-icons/fa";
 
 // type Props = {}
 
 const Header = () => {
-  return (
-    <div>Header component</div>
-  )
-}
+	return (
+		<Navbar className="border-b-2">
+			<Link to="/" className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white">
+				<span className="px-2 py-1 font-bold">MERN</span>
+				blog
+			</Link>
 
-export default Header
+			<form>
+				<TextInput 
+					className="hidden lg:inline"
+					type="text"
+					placeholder="search"
+					rightIcon={AiOutlineSearch}
+				/>
+			</form>
+			
+			<Button className="w-12 h-10 lg:hidden" color="gray">
+				<AiOutlineSearch />
+			</Button>
+
+{/* 
+			<Link to="/" className=" [&.active]:font-bold">
+				<span>MERN</span>
+				blog
+			</Link> */}
+
+		</Navbar>
+	);
+};
+
+export default Header;
