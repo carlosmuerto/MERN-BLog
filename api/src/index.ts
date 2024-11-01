d46af541e975c85884fef70e8fd6572c1c84b10d
Carlos A. Vivas N. <cavivasnieto@gmail.com>
2024-11-01 14:06:16 -0400

    install and connect mongoDB

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,11 +1,23 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import UserService from '@services/UserService';
+import mongoose from "mongoose";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+const mongodbURL = process.env.MONGODB || "";
+
+async function run() {
+  // 4. Connect to MongoDB
+  await mongoose.connect(mongodbURL)
+    .then(() => console.log(`[mongodb]: Mongodb connented to "${mongodbURL}"`))
+    .catch(() => console.log(`[mongodb]: Mongodb cant connent to "${mongodbURL}"`))
+  ;
+}
+
+run().catch(err => console.log(err));
 
 app.get("/", (_: Request, res: Response) => {
   res.send("Express + TypeScript Server");
