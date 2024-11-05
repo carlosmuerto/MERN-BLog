d47c8d0130c8069a821038e7823eea80aa7cf631
Carlos A. Vivas N <cavivasnieto@gmail.com>
2024-11-04 22:47:48 -0400

    use logo component in header

--- client/src/components/header/index.tsx
+++ client/src/components/header/index.tsx
@@ -2,19 +2,14 @@ import { Link } from "@tanstack/react-router";
 import { Button, Navbar, TextInput } from "flowbite-react";
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon } from "react-icons/fa";
+import Logo from "../logo";
 
 // type Props = {}
 
 const Header = () => {
   return (
     <Navbar className="border-b-2">
-      <Link
-        to="/"
-        className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
-      >
-        <span className="px-2 py-1 font-bold">MERN</span>
-        blog
-      </Link>
+      <Logo />
 
       <form>
         <TextInput
