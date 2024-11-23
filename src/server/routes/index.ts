33670a283f5c3aa119e5c7acd7805f1187ebad00
Carlos A. Vivas N <cavivasnieto@gmail.com>
2024-11-22 22:51:59 -0400

    delete not used route after vite migration

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,10 +1,7 @@
 import express from 'express';
-import { defaultRoute } from './default.route';
 import { authRoute } from './auth.route';
 
 const routes = express.Router();
-
-routes.use(defaultRoute);
 routes.use("/auth", authRoute)
 
 export default routes;
--- src/server/routes/user.route.ts
+++
@@ -1,6 +0,0 @@
-import { Router } from 'express';
-import UserController from '@s/controllers/user.controller'
-
-export const userRoute = Router();
-
-// userRoute.get('/', UserController.currentUser);
