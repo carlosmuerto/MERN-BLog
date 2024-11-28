bcaeca1bb6be70d9148a155070dc37aa9015c4ff
Carlos A. Vivas N. <cavivasnieto@gmail.com>
2024-11-28 17:01:24 -0400

    create post model

---
+++ src/server/models/post.model.ts
@@ -0,0 +1,81 @@
+import mongoose, { Document, Schema } from "mongoose";
+import UserSchema, { IUser } from "@s/models/user.model";
+
+// 1. Create an interface representing a document in MongoDB.
+export interface IPost extends Document {
+  author: IUser;
+  title: string;
+  content: string;
+  image: string | null;
+  category: String,
+	slug: String,
+}
+
+// 2. Create a Schema corresponding to the document interface.
+const postSchema = new Schema<IPost>(
+  {
+    author: {
+      type: UserSchema,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    image: {
+      type: String,
+      default:
+        'https://www.hostinger.com/tutorials/wp-content/uploads/sites/2/2021/09/how-to-write-a-blog-post.png',
+    },
+    category: {
+      type: String,
+      default: 'uncategorized',
+    },
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+
+// export interface comatible object
+const toIPostObj = (postdoc: IPost): {
+	author: {
+		id: string;
+		username: string;
+		email: string;
+		profileImg: string | null;
+	},
+  title: string;
+  content: string;
+  image: string | null;
+  category: String,
+	slug: String,
+  
+} => ({
+	author: {
+		id: postdoc.author.id,
+		username: postdoc.author.username,
+		email: postdoc.author.email,
+		profileImg: postdoc.author.profileImg,
+	},
+  title: postdoc.title,
+  content: postdoc.content,
+  image: postdoc.image,
+  category: postdoc.category,
+	slug: postdoc.slug,
+});
+
+export default mongoose.model<IPost>("Post", postSchema);
+
+export { toIPostObj };
