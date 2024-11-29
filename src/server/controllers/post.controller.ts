56c88d1bad9c0800c249dd2950353343b391599d
Carlos A. Vivas N. <cavivasnieto@gmail.com>
2024-11-29 17:44:47 -0400

    remove console log

--- src/server/controllers/post.controller.ts
+++ src/server/controllers/post.controller.ts
@@ -26,7 +26,6 @@ const create = (req: Request, res: Response, next: NextFunction) => {
 };
 
 const queryPosts = (req: Request, res: Response, next: NextFunction) => {
-	console.log(req.query)
 	const page = Number(req.query.page ?? "1")
 	if (!page) return next(new BadRequestError("Bad Request", new Map<string,string>([["page","invalid"]])))
 	return PostService
